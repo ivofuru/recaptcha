e348f0b1f4545ddd82f1957dfe9c8cb9357f0b4b
ivofuru <ivofuru@gmail.com>
2018-11-16 13:47:35 +0300

    fix warning
    warning: found quoted keyword "coveralls" but the quotes are not required...

--- mix.exs
+++ mix.exs
@@ -16,7 +16,7 @@ defmodule Recaptcha.Mixfile do
       # Test coverage:
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
-        "coveralls": :test,
+        coveralls: :test,
         "coveralls.detail": :test,
         "coveralls.post": :test,
         "coveralls.html": :test,
